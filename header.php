c3aca6caa75bfd7b426a8ccb017c0cc7a0aabad8
Rob Penty <robpenty@gmail.com>
2026-04-23 00:20:50 -0400

    Fixing the mobile menu

--- header.php
+++ header.php
@@ -21,12 +21,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
 
-	<!-- Bootstrap core CSS -->
-    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
-
-    <!-- Custom styles for this template -->
-    <!-- <link href="css/styles.css" rel="stylesheet"> -->
-
 	<?php wp_head(); ?>
 </head>
 
@@ -34,46 +28,17 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'abbydylan' ); ?></a>
-
-	<!-- <header id="masthead" class="site-header"> -->
-		<!-- <div class="site-branding"> -->
-			<?php
-			//the_custom_logo();
-			//if ( is_front_page() && is_home() ) :
-				?>
-				<!-- <h1 class="site-title"><a href="<?php //echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //bloginfo( 'name' ); ?></a></h1> -->
-				<?php
-			//else :
-				?>
-				<!-- <p class="site-title"><a href="<?php //echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //bloginfo( 'name' ); ?></a></p> -->
-				<?php
-			//endif;
-			//$abbydylan_description = get_bloginfo( 'description', 'display' );
-			//if ( $abbydylan_description || is_customize_preview() ) :
-				?>
-				<!-- <p class="site-description"><?php //echo $abbydylan_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p> -->
-			<?php //endif; ?>
-		<!-- </div> -->
-		<!-- .site-branding -->
-
-		<!-- <nav id="site-navigation" class="main-navigation"> -->
-			<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'abbydylan' ); ?></button> -->
-			<?php
-			// wp_nav_menu(
-			// 	array(
-			// 		'theme_location' => 'menu-1',
-			// 		'menu_id'        => 'primary-menu',
-			// 	)
-			// );
-			?>
-		<!-- </nav> -->
-		<!-- #site-navigation -->
-	<!-- </header> -->
 	<!-- #masthead -->
 	<div class="cover-container d-flex h-100 p-3 mx-auto flex-column"> <!-- .cover-container -->
 		<header class="masthead">
         	<div class="inner">
-				<nav class="nav nav-masthead justify-content-center">
+				<nav id="site-navigation" class="nav-masthead justify-content-md-center">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+						<span class="hamburger-bar"></span>
+						<span class="hamburger-bar"></span>
+						<span class="hamburger-bar"></span>
+						<span class="screen-reader-text"><?php esc_html_e( 'Menu', 'abbydylan' ); ?></span>
+					</button>
 					<?php
 					wp_nav_menu(
 						array(
